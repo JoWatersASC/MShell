b1708d7ee829d199b70a372dca3078b9a1cf6951
JoWatersASC <joshua.waters365@gmail.com>
2025-01-17 10:23:43 -0500

    Added rel_path function for resolving relative path names, changed paths to relative paths instead of raw args, ls fixed by way of specifying relative paths and will list files & not just directories

--- src/built_ins/built_ins.c
+++ src/built_ins/built_ins.c
@@ -1,5 +1,33 @@
 #include "shell.h"
 
+char* rel_path(char* _name, char* _path) { // return string '_path' (or './' if NULL) with '_name' appended to it so the shell can see relative to current dir
+	char* path;
+
+	if(_path) {
+		path = (char *)malloc(strlen(_name) + strlen(_path) + 3);
+	} else {
+		path = (char *)malloc(strlen(_name) + 2);
+	}
+	if(!path) {
+		MSHERR("Allocation error")
+		return NULL;
+	}
+
+	path[0] = '.';
+	path[1] = '/';
+	path[2] = '\0';
+
+	if(_path) {
+		strcat(path, _path);
+		strcat(path, "/");
+		strcat(path, _name);
+	} else {
+		strcat(path, _name);
+	}
+
+	return path;
+}
+
 int msh_cd(char** args) {
     if(!args[1]) {
         MSHERR("cd: Too few arguments")
@@ -7,10 +35,19 @@ int msh_cd(char** args) {
         MSHERR("cd: Too many arguments")
         return 0;
     }else {
-        if(chdir(args[1])) {
+		char* path = rel_path(args[1], NULL);
+		if(!path) {
+			MSHERR("Allocation error")
+			return 0;
+		}
+
+        if(chdir(path)) {
             perror("MSH: chdir error");
+			free(path);
             return 0;
         }
+		free(path);
+
         char pwd[128];
         getcwd(pwd, sizeof(pwd));
 
@@ -22,36 +59,72 @@ int msh_cd(char** args) {
 }
 
 int msh_ls(char** args) {
-	if(!args[1]) {
-		int fd = open(".", O_DIRECTORY | O_RDONLY);
-		if(fd == -1) {
-			MSHERR(strerror(errno))
+	int fd;
+	char* path = NULL;
+
+	if(args[1]) {
+		if(args[2]) {
+			MSHERR("ls: Too many arguments")
 			return 0;
 		}
-	
-		DIR* dir = fdopendir(fd);
-		if(!dir) {
-			MSHERR(strerror(errno))
+		
+		path = rel_path(args[1], NULL);
+		printf("%s\n", path);
+		if(!path) {
+			MSHERR("Allocation error")
 			return 0;
 		}
-		struct dirent* entry;
 
-		while((entry = readdir(dir)) != NULL) {
-			unsigned short rlen = entry->d_reclen;
-			
-			if(entry->d_type == DT_DIR) {
-				printf("\033[34m");
-			}
-
-			if(*entry->d_name != '.') {
-				printf("%s ", entry->d_name);
-			}
-			printf("\033[0m");
-		}
-
-		printf("\n");
-		close(fd);
+		fd = open(path, O_DIRECTORY | O_RDONLY);
+	} else {
+		fd = open(".", O_DIRECTORY | O_RDONLY);
 	}
+
+	if(fd == -1) {
+		MSHERR(strerror(errno))
+		return 0;
+	}
+
+	DIR* dir = fdopendir(fd);
+	if(!dir) {
+		MSHERR(strerror(errno))
+		close(fd);
+		return 0;
+	}
+	
+	struct dirent* entry;
+
+	while((entry = readdir(dir)) != NULL) {
+		unsigned short rlen = entry->d_reclen;
+
+		if(entry->d_type == DT_DIR) {
+			printf("\033[34m");
+		} else {
+			struct stat stat_buff;
+			char* e_name = rel_path(entry->d_name, path);
+
+			if(stat(e_name, &stat_buff) == -1) {
+				MSHERR(strerror(errno))
+				close(fd);
+				return 0;
+			} else {
+				if(stat_buff.st_mode & (S_IXUSR | S_IXOTH)) {
+					printf("\033[38;5;87m");
+				}
+			}
+
+			free(e_name);
+		}
+
+		if(*entry->d_name != '.') {
+			printf("%s ", entry->d_name);
+		}
+		printf("\033[0m");
+	}
+
+	printf("\n");
+	close(fd);
+
 	return 1;
 }
 
