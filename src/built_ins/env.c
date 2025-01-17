c3091ff11a1f24374f6d6868fb57075dc8ba8672
JoWatersASC <joshua.waters365@gmail.com>
2025-01-17 10:25:32 -0500

    Outlined requirements for 'msh_alias', 'msh_unalias', 'msh_source' functions

--- src/built_ins/env.c
+++ src/built_ins/env.c
@@ -40,12 +40,35 @@ int msh_unset(char** args) {
 }
 
 int msh_alias(char** args) {
+	// make hashmap from char* to char* for alias list
 	return 1;
 }
 int msh_unalias(char** args) {
+	// make delete function for char*, char* alias hashmap
 	return 1;
 }
 
 int msh_source(char** args) {
+	if(!args[1]) {
+		MSHERR("source: Too few arguments")
+		return 0;
+	}
+	if(args[2]) {
+		MSHERR("source: Too many arguments")
+		return 0;
+	}
+
+// read lines and run commands
+  // open file
+	if(fopen(args[1], "r") == NULL) {
+		MSHERR(strerror(errno))
+		return 0;
+	}
+  // read line into buffer or return if eof
+	
+  // parse line buffer
+  // run command
+  // repeat
+  // close file
 	return 1;
 }
