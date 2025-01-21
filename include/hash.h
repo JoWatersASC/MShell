bf57cebd0c4c537b6b1760a50199c1298d19ef6a
JoWatersASC <joshua.waters365@gmail.com>
2025-01-21 15:20:00 -0500

    Added default hash function, string-string pair structure, string-string map, and map insert, retrieval, and utility functions

---
+++ include/hash.h
@@ -0,0 +1,36 @@
+#ifndef JW_HASH_H
+#define JW_HASH_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "shell.h"
+
+#define INF 0x3F3F3F3F
+
+extern inline unsigned int oat_hashf(void *val, const int len);
+
+typedef struct sspair { // string-string pair
+    const char* key;
+    char* val;
+    struct sspair* next;
+} sspair;
+typedef struct ssmap { // string-string map
+    struct sspair** data;
+    unsigned int size;
+    unsigned int cap;
+} ssmap;
+
+extern inline ssmap* make_ssmap(unsigned int _cap);
+
+sspair* get_ssp(ssmap *, const char *);
+char* get_ssval(ssmap *, const char *);
+
+bool ssminsert(ssmap *, sspair);
+bool ssminsertss(ssmap *, char *, char *);
+bool ssminsertp(ssmap *, sspair *);
+
+bool ssrehash(ssmap*, unsigned int, unsigned int (*) (void *, int));
+
+#endif
